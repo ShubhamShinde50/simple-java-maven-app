4ac25a0d967f81d3406c01128e786985ec9c0cc2
ShubhamShinde50 <131276052+ShubhamShinde50@users.noreply.github.com>
2023-07-02 12:39:28 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World!";
+    private static final String MESSAGE = "Hello World! /n WeCome to Jenkins";
 
     public App() {}
 
